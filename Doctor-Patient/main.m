011dfeb04f8a208be1a092669c281c2aa64e1b88
Chelsea Liu <cl314@duke.edu>
2015-06-11 11:26:01 -0400

    Initial Commit

---
+++ Doctor-Patient/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Doctor-Patient
+//
+//  Created by Chelsea Liu on 6/11/15.
+//  Copyright (c) 2015 Chelsea Liu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
